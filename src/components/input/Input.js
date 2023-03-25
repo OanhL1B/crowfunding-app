b4edc92b7f74ad7933bd2408b663dcadcb4c1658
OanhL1B <vuthihongoanh1803@gmail.com>
2023-03-25 22:17:38 +0700

    4. SignUp sesson 4

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -3,7 +3,7 @@ import { useController } from "react-hook-form";
 import PropTypes from "prop-types";
 
 const Input = (props) => {
-  const { control, name, type } = props;
+  const { control, name, type = "text", ...rest } = props;
   const { field } = useController({
     control,
     name,
@@ -12,8 +12,10 @@ const Input = (props) => {
   return (
     <div className="relative">
       <input
+        id="name" // khi bấm vào sẽ focus vào ô bên dưới
         type={type}
         className="w-full px-6 py-4 text-sm font-medium border rounded-xl border-strock text-text1 placeholder:text-text-4"
+        {...rest}
         {...field}
       />
     </div>
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
-import { Input } from "../components/input";
-import { Label } from "../components/label";
-import LayoutAuthentication from "../layouts/LayoutAuthentication";
+import { Input } from "components/input";
+import { Label } from "components/label";
+import LayoutAuthentication from "layouts/LayoutAuthentication";
+import FormGroup from "components/common/FormGroup";
+import { Button } from "components/button";
 
 const SignUpPage = () => {
-  const { handleSubmit, control } = useForm({});
+  const {
+    handleSubmit,
+    control,
+    formState: { isValid, isSubmitting },
+  } = useForm({});
+  // bắt sự kiện onsubmit
+  const handleSignUp = (values) => {};
   return (
     <LayoutAuthentication heading="SignUp">
       <p className="mb-6 text-xs font-normal text-center lg:text-sm text-text3 lg:mb-8">
@@ -22,11 +30,41 @@ const SignUpPage = () => {
       <p className="mb-4 text-xs font-normal text-center lg:text-sm lg:mb-8 text-text2">
         Or sign up with email
       </p>
-      <form>
-        <div className="flex flex-col gap-x-3">
-          <Label>Full Name *</Label>
-          <Input control={control} name="Full Name *"></Input>
+      <form onSubmit={handleSubmit(handleSignUp)}>
+        <FormGroup>
+          <Label htmlFor="name">Full Name *</Label>
+          <Input control={control} name="name" placeholder="Jhon Doe"></Input>
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="email">Email *</Label>
+          <Input
+            control={control}
+            name="email"
+            placeholder="example@gmail.com"
+            type="email"
+          ></Input>
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="password">Password *</Label>
+          <Input
+            control={control}
+            name="password"
+            placeholder="create a password"
+            type="password"
+          ></Input>
+        </FormGroup>
+        <div className="flex mb-5 item-start gap-x-5">
+          <span className="inline-block w-5 h-5 border rounded border-text4"></span>
+          <p className="flex-1 text-sm text-text2">
+            I agree to the{" "}
+            <span className="underline text-secondary">Terms of Use </span> and
+            have read and understand the{" "}
+            <span className="underline text-secondary">Privacy policy.</span>
+          </p>
         </div>
+        <Button className="w-full bg-primary" type="submit">
+          Create my account
+        </Button>
       </form>
     </LayoutAuthentication>
   );
