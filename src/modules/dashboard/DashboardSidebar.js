42268df28b1e6aa80a57245e7c186b56ce40eaa0
OanhL1B <vuthihongoanh1803@gmail.com>
2023-04-19 21:05:07 +0700

    11 Side bar

--- src/modules/dashboard/DashboardSidebar.js
+++ src/modules/dashboard/DashboardSidebar.js
@@ -1,7 +1,92 @@
+import {
+  IconCampaign,
+  IconDarkmode,
+  IconDashboard,
+  IconLogout,
+  IconPayment,
+  IconProfile,
+  IconWithdraw,
+} from "components/icons";
 import React from "react";
+import { NavLink } from "react-router-dom";
 
+// mảng chưa item và icon
+const sidebarLinks = [
+  {
+    icon: <IconDashboard></IconDashboard>,
+    title: "Dashboard",
+    url: "/",
+  },
+  {
+    icon: <IconCampaign></IconCampaign>,
+    title: "Campaign",
+    url: "/campaign",
+  },
+  {
+    icon: <IconPayment></IconPayment>,
+    title: "Payment",
+    url: "/payment",
+  },
+  {
+    icon: <IconWithdraw></IconWithdraw>,
+    title: "Withdraw",
+    url: "/withdraw",
+  },
+  {
+    icon: <IconProfile></IconProfile>,
+    title: "Profile",
+    url: "/profile",
+  },
+  {
+    icon: <IconLogout></IconLogout>,
+    title: "Logout",
+    url: "/logout",
+  },
+  {
+    icon: <IconDarkmode></IconDarkmode>,
+    title: "Light/Dark",
+    url: "/#",
+    onClick: () => {},
+  },
+];
 const DashboardSidebar = () => {
-  return <div>Side bar</div>;
+  const navlinkClass =
+    "flex items-center gap-x-5 md:w-12 md:h-12 md:justify-center md:rounded-lg md:mb-8  last:mt-auto last:bg-white last:shadow-sdprimary";
+  // const dispatch = useDispatch();
+  return (
+    <div className="w-full md:w-[76px] rounded-3xl bg-white shadow-[10px_10px_20px_rgba(218,_213,_213,_0.15)] px-[14px] py-10 flex flex-col flex-shrink-0">
+      {/* duyệt qua sidebarLink để lấy được từng field nè */}
+      {sidebarLinks.map((link) => {
+        if (link.url === "/logout") {
+          return (
+            <button
+              // onClick={() => dispatch(authLogOut())}
+              className={navlinkClass}
+              key={link.title}
+            >
+              <span>{link.icon}</span>
+              <span className="md:hidden">{link.title}</span>
+            </button>
+          );
+        }
+        return (
+          // dùng navlink để xài active đó
+          <NavLink
+            to={link.url}
+            key={link.title}
+            className={({ isActive }) =>
+              isActive
+                ? `${navlinkClass} text-primary bg-primary bg-opacity-20`
+                : `${navlinkClass} text-icon-color`
+            }
+          >
+            <span>{link.icon}</span>
+            <span className="md:hidden">{link.title}</span>
+          </NavLink>
+        );
+      })}
+    </div>
+  );
 };
 
 export default DashboardSidebar;
